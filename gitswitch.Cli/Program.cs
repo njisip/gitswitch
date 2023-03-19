97b87e1f5f109ba8971606f3733f4e0ed7dd4c1f
Nards John Isip <ndisip@up.edu.ph>
2023-03-19 21:55:14 +0800

    Create and configure console project

---
+++ gitswitch.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitswitch.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gitswitch");
+        }
+    }
+}
