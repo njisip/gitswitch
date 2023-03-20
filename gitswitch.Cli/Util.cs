724a66f9ddbffcc2faaf6a75d757f65240731e3c
Nards John Isip <ndisip@up.edu.ph>
2023-03-20 22:13:44 +0800

    Show placeholder if name or email is not set

--- gitswitch.Cli/Util.cs
+++ gitswitch.Cli/Util.cs
@@ -2,6 +2,8 @@
 {
     internal static class Util
     {
+        private static string _notSet = "<not set>";
+
         /// <summary>
         /// Shows a user name and email.
         /// </summary>
@@ -9,6 +11,9 @@
         /// <param name="email">The user email.</param>
         internal static void ShowUser(string name, string email)
         {
+            name = string.IsNullOrEmpty(name) ? _notSet : name;
+            email = string.IsNullOrEmpty(email) ? _notSet : email;
+
             Console.WriteLine($"Name:\t{name}");
             Console.WriteLine($"Email:\t{email}");
         }
