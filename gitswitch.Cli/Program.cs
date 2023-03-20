1421904ceb373bc05d7e6033526263060a183db4
Nards John Isip <ndisip@up.edu.ph>
2023-03-20 22:36:53 +0800

    Implement loading of existing users

--- gitswitch.Cli/Program.cs
+++ gitswitch.Cli/Program.cs
@@ -1,14 +1,22 @@
 ﻿using gitswitch.Cli.Commands;
 using System.CommandLine;
+using System.Text.Json;
 
 namespace gitswitch.Cli
 {
     internal class Program
     {
+        private static string _usersPath = "users.json";
+
         /// <summary>
         /// The root command: gitsw.
         /// </summary>
         private static RootCommand? _rootCommand;
+
+        /// <summary>
+        /// The list of users.
+        /// </summary>
+        private static Dictionary<string, User>? _users;
 
         /// <summary>
         /// The starting point of the application.
@@ -17,11 +25,16 @@ namespace gitswitch.Cli
         /// <returns>The exit code.</returns>
         static async Task<int> Main(string[] args)
         {
+            // Check if git is installed
             if (!Git.IsExist())
             {
                 Console.WriteLine("git is not installed on your system");
                 return 1;
             }
+
+            // Load users
+            var json = File.ReadAllText(_usersPath);
+            _users = JsonSerializer.Deserialize<Dictionary<string, User>>(json)!;
 
             // Create root commnand
             _rootCommand = new RootCommand("A CLI tool to easily switch between users in a repository");
